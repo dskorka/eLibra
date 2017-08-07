f059ef7e2e12d6bb4f0bd2f88b1be0178866955d
Damian <damian.skorka@gmail.com>
2017-08-07 21:20:53 +0200

    set h2 console

--- eLibraApi/src/main/java/infrastructure/config/H2Config.java
+++ eLibraApi/src/main/java/infrastructure/config/H2Config.java
@@ -38,4 +38,25 @@ public class H2Config {
                 .addScript("db/createstructure/insert_data.sql")
                 .build();
     }
+
+    /**
+     * TCP connection to connect with SQL clients to the embedded h2 database.
+     *
+     * Connect to "jdbc:h2:tcp://localhost:9092/mem:testdb", username "sa", password empty.
+     */
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+
+
+    /**
+     * Web console for the embedded h2 database.
+     *
+     * Go to http://localhost:8082 and connect to the database "jdbc:h2:mem:testdb", username "sa", password empty.
+     */
+    @Bean(destroyMethod = "stop")
+    public Server h2WebServer() throws SQLException {
+        return Server.createWebServer("-webAllowOthers","-webPort","8082").start();
+    }
 }
